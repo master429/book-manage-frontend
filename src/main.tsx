39b210e190ce426f79ebe6b6d6f58818657480a4
liuqingpo <1432003391@qq.com>
2024-10-07 17:47:59 +0800

    feat:完成前端的注册和登录页面以及接口调用

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BookManage } from "./BookManage";
+import { Login } from "./Login";
+import { Register } from "./Register";
+
+const routes = [
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/",
+    element: <BookManage />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
